f7cffef7d07d4af7367d024649e41dd3f2754bb9
reidmalone <reidmalone@gmail.com>
2019-03-30 16:12:50 -0500

    added current key

--- transposingTabs/src/app/text-area/text-area.component.ts
+++ transposingTabs/src/app/text-area/text-area.component.ts
@@ -14,4 +14,34 @@ export class TextAreaComponent {
   logText(value: string): void {
     this.log += `'${value}'\n`;
   }
+
+}
+
+class DropdownMenuButton {
+  dropdownMenuButton: HTMLSelectElement;
+  options2: HTMLOptionsCollection;
+
+  constructor(dropdown: HTMLSelectElement) {
+      this.dropdownMenuButton = dropdown;
+      this.options2 = dropdown.options;
+      }
+
+
+  OnClick() {
+      alert(this.options2[this.options2.selectedIndex].value);
+      //if (this.options2.selectedIndex >= 1)
+          //document.location.href = this.options2[this.options2.selectedIndex].value;
+  }
+
+}
+
+
+window.onload = () => {
+  let select2: HTMLSelectElement = <HTMLSelectElement>document.getElementById("currentKey");
+  let btngo: HTMLButtonElement = <HTMLButtonElement>document.getElementById("selectKey");
+  let ddm2: DropdownMenuButton = new DropdownMenuButton(select2);
+  btngo.onclick = () => {
+      ddm2.OnClick();
+  }
+
 }
