7f75a9facaa77cfc2e96f72c0384c159b339ba70
Jon <jonathanbouligny@protonmail.com>
2019-04-08 11:08:04 -0500

    Fiddled with home css and added html code to chords view

--- transposingTabs/src/app/views/chords/chords.component.ts
+++ transposingTabs/src/app/views/chords/chords.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-chords',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChordsComponent implements OnInit {
 
-  constructor() { }
+  newChords: any;
+  currentSong:string;
+  currentAlbum:string;
+  currentArtist:string;
+
+  constructor(private _Activatedroute:ActivatedRoute,private dataService: DataService) { }
 
   ngOnInit() {
+    //songName": songName, "songArtist": songArtist,"songAlbum":songAlbum,"songKey":songKey
+    this.getSongChords(this._Activatedroute.snapshot.params['songName'],
+    this._Activatedroute.snapshot.params['songArtist'],
+    this._Activatedroute.snapshot.params['songAlbum']
+    ,this._Activatedroute.snapshot.params['songKey'])
   }
 
+  getSongChords(songName:string,songArtist:string,songAlbum:string,songKey:string){
+    this.dataService.getTabsForSong(songName,songArtist,songAlbum,songKey).subscribe( (value) => {
+      this.newChords = value.Tab;
+      this.currentSong = songName;
+      this.currentAlbum = songAlbum;
+      this.currentArtist = songArtist;
+    });
+  }
 }
